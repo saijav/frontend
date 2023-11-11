4b45be31b4a9cbf59bb27df938877b062fed4483
Saija Vittaniemi <saijavittaniemi@Saija-Air.lan>
2023-11-11 15:53:58 +0200

    Deleted feature

--- first_game/script.js
+++ first_game/script.js
@@ -1,28 +1,11 @@
 var character = document.getElementById("character");
 var block = document.getElementById("block");
-var characterLeftArm = document.getElementById("characterLeftArm");
-var characterRightArm = document.getElementById("characterRightArm");
 function jump(){
     if(character.classList != "animate"){
         character.classList.add("animate");
     }
     setTimeout(function(){
         character.classList.remove("animate");
-    }, 500);
-}
-
-function moveHands(){
-    if(characterLeftArm.classList != "moveHand"){
-        characterLeftArm.classList.add("moveHand");
-    }
-    setTimeout(function(){
-        characterLeftArm.classList.remove("moveHand");
-    }, 500);
-    if(characterRightArm.classList != "moveHand"){
-        characterRightArm.classList.add("moveHand");
-    }
-    setTimeout(function(){
-        characterRightArm.classList.remove("moveHand");
     }, 500);
 }
 var checkDead = setInterval(function(){
